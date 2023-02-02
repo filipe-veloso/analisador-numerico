62cff9ae852858986db1138111368442b3a2982e
filipe-veloso <filipev.amaral@gmail.com>
2023-02-02 10:24:50 -0300

    erro gramática

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ function finalizar(){
     res.innerHTML = ''
     res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados</p>`
     res.innerHTML +=`<p>O maior valor informado foi ${maior}</p>`
-    res.innerHTML +=`<p>O menos valor informado foi ${menor}</p>`
+    res.innerHTML +=`<p>O menor valor informado foi ${menor}</p>`
     res.innerHTML +=`<p>Somando todos os valores temos ${soma}</p>`
     res.innerHTML +=`<p>A média dos valores digitados é ${media}</p>`
 
